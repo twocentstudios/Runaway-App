09f72b9735e3e8e339115933c4d6ba6fe84048b0
Christopher Trott <chris@twocentstudios.com>
2016-06-03 18:28:41 -0500

    Initial Commit

---
+++ processalert/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  processalert
+//
+//  Created by Christopher Trott on 6/3/16.
+//  Copyright © 2016 twocentstudios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
