993cfe355c7e681f116f7dd59c65fd9a8d300a88
Christopher Trott <chris@twocentstudios.com>
2016-06-07 00:15:43 -0500

    clean up notification

--- processalert/AppDelegate.swift
+++ processalert/AppDelegate.swift
@@ -20,10 +20,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
     
-    let numberOfAveragedSamples = 10
-    let cpuThreshold = 10.0
+    let numberOfSamples = 6
+    let cpuThreshold = 50.0
     let delay: NSTimeInterval = 3 // seconds
-    let remainingSamples = 10
+    let remainingSamples = 30
+    let alertThresholdMinutes = 30
     var processes: ProcessHash = [:]
     
     var timer: dispatch_source_t!
@@ -36,7 +37,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        dispatch_source_cancel(timer);
     }
     
     private func shell(launchPath: String, arguments: [String]) -> String {
@@ -90,34 +91,31 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let psOutput = shell("/bin/ps", arguments: ["-acwwwxo", "pid=,pcpu=,command="])
         let rawProcesses = parseProcessStatus(psOutput)
         let mergedProcesses = mergeRawProcesses(rawProcesses, processes: processes)
-        let processAverageCPU = averageLatestSamples(mergedProcesses, numberOfSamples: 10)
-        let filtered = filterAverages(processAverageCPU, threshold: cpuThreshold)
+        let filtered = filterLatestSamples(mergedProcesses, numberOfSamples: numberOfSamples, threshold: cpuThreshold)
         let updatedProcesses = sendNotifications(mergedProcesses, overProcesses: filtered, notificationCenter: notificationCenter)
         let prunedSamplesFromProcesses = pruneSamples(updatedProcesses, remainingSamples: remainingSamples)
         return prunedSamplesFromProcesses
     }
     
-    private func averageLatestSamples(processes: ProcessHash, numberOfSamples: Int) -> [ProcessID: CPUPercentage] {
+    // Returns the average load for processes where all latest samples are above the provided threshold.
+    private func filterLatestSamples(processes: ProcessHash, numberOfSamples: Int, threshold: CPUPercentage) -> [ProcessID: CPUPercentage] {
         let processLoadArray = processes
             .map { (processId: ProcessID, process: Process) -> (ProcessID, [CPUPercentage]) in
                 let truncatedSamples = Array(process.samples.suffix(numberOfSamples))
                 return (processId, truncatedSamples)
             }
             .filter({ $1.count == numberOfSamples })
+            .filter({ (processID: ProcessID, samples: [CPUPercentage]) -> Bool in
+                let allSamplesGreaterThanThreshold = samples.reduce(true, combine: { $0 && ($1 >= threshold)})
+                return allSamplesGreaterThanThreshold
+            })
             .map { (processID: ProcessID, samples: [CPUPercentage]) -> (ProcessID, CPUPercentage) in
                 let total = samples.reduce(0, combine: +)
                 let count = samples.count
                 let average = total / Double(count)
                 return (processID, average)
-        }
+            }
         return Dictionary(processLoadArray)
-    }
-    
-    private func filterAverages(averages: [ProcessID: CPUPercentage], threshold: CPUPercentage) -> [ProcessID: CPUPercentage] {
-        let filteredAveragesArray = averages.filter({ (process: ProcessID, percentage: CPUPercentage) -> Bool in
-            return percentage >= threshold
-        })
-        return Dictionary(filteredAveragesArray)
     }
     
     private func getProcessName(processes: ProcessHash, processId: ProcessID) -> String {
@@ -130,32 +128,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         var updatedProcesses = processes
         var pendingNotifications: [NSUserNotification] = []
         for (processId, cpuLoad) in overProcesses {
+            guard var process = processes[processId] else { print("Error finding runaway process \(processId)"); continue }
+            if !process.shouldSendNotification(now, thresholdMinutes: alertThresholdMinutes) { print("Ignored alert for process \(processId)"); continue }
+            
             // Configure local notification
             let notification = createUserNotification(
                 getProcessName(processes, processId: processId),
-                averagedTimePeriod: delay * Double(numberOfAveragedSamples),
+                averagedTimePeriod: delay * Double(numberOfSamples),
                 cpuLoad: cpuLoad)
             pendingNotifications.append(notification)
             
             // Update process lastAlertAt
-            var process = processes[processId]
-            process?.lastAlertAt = now
+            process.lastAlertAt = now
             updatedProcesses[processId] = process
             
-            // print(processes[processId])
+            print(processes[processId])
         }
         
-        // Schedule notifications
-        pendingNotifications.forEach { notificationCenter.scheduleNotification($0) }
+        // Deliver notifications
+        pendingNotifications.forEach { notificationCenter.deliverNotification($0) }
         
         return updatedProcesses
     }
     
     private func createUserNotification(processName: String, averagedTimePeriod: NSTimeInterval, cpuLoad: CPUPercentage) -> NSUserNotification {
-        let notificationString = "\(processName) has been using \(cpuLoad) CPU for \(averagedTimePeriod) seconds."
+        let notificationTitle = "\(processName) using over \(cpuThreshold)% CPU"
+        let notificationText = "Average \(cpuLoad)% CPU for the last \(Int(averagedTimePeriod)) seconds."
         let notification = NSUserNotification()
-        notification.title = "Runaway Process Monitor"
-        notification.informativeText = notificationString
+        notification.title = notificationTitle
+        notification.informativeText = notificationText
         return notification
     }
     
@@ -170,21 +171,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     private func repeatingTimer(interval: NSTimeInterval, leeway: NSTimeInterval, start: dispatch_time_t = DISPATCH_TIME_NOW, queue: dispatch_queue_t = dispatch_get_main_queue(), action: dispatch_block_t) -> dispatch_source_t {
-            let timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue)
-            guard timer != nil else { fatalError("Could not create timer.") }
+        let timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue)
+        guard timer != nil else { fatalError("Could not create timer.") }
+        
+        func nanoseconds(seconds: NSTimeInterval) -> UInt64 {
+            enum Static { static let Multiplier = Double(NSEC_PER_SEC) }
             
-            func nanoseconds(seconds: NSTimeInterval) -> UInt64 {
-                enum Static { static let Multiplier = Double(NSEC_PER_SEC) }
-                
-                return UInt64(seconds * Static.Multiplier)
-            }
-            
-            dispatch_source_set_event_handler(timer, action)
-            dispatch_source_set_timer(timer, start, nanoseconds(interval), nanoseconds(leeway))
-            
-            dispatch_resume(timer)
-            
-            return timer
+            return UInt64(seconds * Static.Multiplier)
+        }
+        
+        dispatch_source_set_event_handler(timer, action)
+        dispatch_source_set_timer(timer, start, nanoseconds(interval), nanoseconds(leeway))
+        
+        dispatch_resume(timer)
+        
+        return timer
     }
 }
 
@@ -217,6 +218,12 @@ extension Process {
         if remainingSamples <= 0 { return }
         if self.samples.count <= remainingSamples { return }
         self.samples = Array(self.samples.suffix(remainingSamples))
+    }
+    
+    func shouldSendNotification(currentTime: NSDate, thresholdMinutes: Int) -> Bool {
+        guard let lastAlertAt = self.lastAlertAt else { return true }
+        let thresholdSeconds = NSTimeInterval(thresholdMinutes * 60)
+        return currentTime.timeIntervalSinceDate(lastAlertAt) > thresholdSeconds
     }
 }
 
